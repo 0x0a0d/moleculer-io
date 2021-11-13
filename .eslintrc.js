307251974c518d68d2f7e6e8f2248c9b3c24ca2f
0x0a0d <vandug23@gmail.com>
2021-11-13 20:25:50 +0700

    internal release 1.1.3

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
 		jasmine: false
 	},
 	extends: ["eslint:recommended", "plugin:security/recommended", "plugin:prettier/recommended"],
+	parser: "@typescript-eslint/parser",
 	parserOptions: {
 		sourceType: "module",
 		ecmaVersion: "2018"
@@ -23,5 +24,19 @@ module.exports = {
 		"security/detect-non-literal-require": ["off"],
 		"security/detect-non-literal-fs-filename": ["off"],
 		"node/no-unpublished-require": 0
-	}
+	},
+	overrides: [
+		// typescript
+		{
+			files: ["*.ts", "*.tsx"],
+			parser: "@typescript-eslint/parser",
+			plugins: ["@typescript-eslint"],
+			extends: ["plugin:@typescript-eslint/recommended"],
+			rules: {
+				"@typescript-eslint/ban-ts-comment": "off",
+				"@typescript-eslint/no-explicit-any": "off",
+				"@typescript-eslint/explicit-module-boundary-types": "off"
+			}
+		}
+	]
 };
